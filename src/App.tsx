9ba693f8d2a1150f6c44eeb634eb252e861c4d56
André Luiz Duarte Cruz <44853890+andrelDuarte@users.noreply.github.com>
2021-07-28 13:39:05 -0300

    adding update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
- import { SideBar } from './components/SideBar';
- import { Content } from './components/Content';
-
-import { api } from './services/api';
-
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 import './styles/global.scss';
-
 import './styles/sidebar.scss';
 import './styles/content.scss';
 import { MoviesProvider } from './MoviesContext';
@@ -24,7 +16,7 @@ export function App() {
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <SideBar />
       <Content />
-            </div>
+      </div>
     </MoviesProvider>
      )
 }
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,10 +2,10 @@ import { useMovies } from "../MoviesContext";
 import { MovieCard } from "./MovieCard";
 
 export function Content() {
+  const {selectedGenre, movies} = useMovies()
 
-  const { selectedGenre, movies} = useMovies();
-
-  <div className="container">
+  return (
+    <div className="container">
         <header>
           <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
         </header>
@@ -13,9 +13,10 @@ export function Content() {
         <main>
           <div className="movies-list">
             {movies.map(movie => (
-              <MovieCard key ={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+              <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
             ))}
           </div>
         </main>
       </div>
+  )
 }
